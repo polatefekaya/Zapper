c6d5c2f27cada736c3f93e21473a91e334e4b7a9
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-07-06 14:48:54 +0300

    gitignore added

--- realTimeApp.Application/obj/Debug/net8.0/realTimeApp.Application.AssemblyInfo.cs
+++ realTimeApp.Application/obj/Debug/net8.0/realTimeApp.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("realTimeApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c9652b034d05ff21abd8badb131b3422354197")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dead59e89baabad95d92f841009e08410441aa9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("realTimeApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("realTimeApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
