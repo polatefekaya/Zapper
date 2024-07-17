d8e49e1c73e47a0a1581ed1a6c7c48fad9f721a7
polatefekaya <polatsfekaya@gmail.com>
2024-07-18 00:01:40 +0300

    Messaging through cli with groups is working

--- client/realTimeApp.Client.Application/Interfaces/IMessagingCommandsService.cs
+++ client/realTimeApp.Client.Application/Interfaces/IMessagingCommandsService.cs
@@ -1,6 +1,10 @@
-﻿namespace realTimeApp.Client.Application.Interfaces;
+﻿using realTimeApp.Client.Domain.Data.Entities;
+
+namespace realTimeApp.Client.Application.Interfaces;
 
 public interface IMessagingCommandsService
 {
     Task<int> MessageCommand(string command);
+    Task<int> SendMessageToGroup();
+    Task SendMessage(string identifier, MessageEntity message);
 }
--- client/realTimeApp.Client.Application/Services/MessagingCommandsService.cs
+++ client/realTimeApp.Client.Application/Services/MessagingCommandsService.cs
@@ -7,9 +7,11 @@ namespace realTimeApp.Client.Application.Services;
 public class MessagingCommandsService : IMessagingCommandsService
 {
     private readonly IMessageService _messageService;
+    private readonly ISessionManagerService _sessionManagerService;
 
-    public MessagingCommandsService(IMessageService messageService){
+    public MessagingCommandsService(IMessageService messageService, ISessionManagerService sessionManagerService){
         _messageService = messageService;
+        _sessionManagerService = sessionManagerService;
     }
     public async Task<int> MessageCommand(string command)
     {
@@ -19,8 +21,10 @@ public class MessagingCommandsService : IMessagingCommandsService
         //Classify Commands
         switch(command){
             case "group":
+                exitType = await SendMessageToGroup();
                 break;
             case "user":
+                exitType = 2;
                 break;
             case "exit":
                 exitType = 1;
@@ -32,15 +36,35 @@ public class MessagingCommandsService : IMessagingCommandsService
         return exitType;
     }
 
-    private async Task SendMessageToGroup(string groupName,string command){
+    public async Task<int> SendMessageToGroup(){
+        string? sessionName = _sessionManagerService.GetCurrentSession();
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(sessionName);
+
+        int exitType = 0;
         while(true){
-            switch(command){
-                //Implement
+            string? line = Console.ReadLine();
+            if(line is not null){
+                switch(line){
+                    case "exit":
+                    exitType = 1;
+                    break;
+                    case "..":
+                    exitType = 2;
+                    break;
+                    default:
+                        MessageEntity message = new MessageEntity{
+                            Body = line
+                        };
+                        await SendMessage(sessionName, message);
+                    break;
+                }
+                if(exitType == 1 || exitType == 2) break;
             }
         }
-        MessageEntity message = new MessageEntity{
-            Body = command
-        };
-        await _messageService.SendMessageToGroup(groupName, message);
+        return exitType;
+    }
+
+    public async Task SendMessage(string identifier, MessageEntity message){
+        await _messageService.SendMessageToGroup(identifier, message);
     }
 }
