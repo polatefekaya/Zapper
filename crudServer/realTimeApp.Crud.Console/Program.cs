af82d0c1da8e47cc904a7609d4f9bb5a1f00359c
polatefekaya <polatsfekaya@gmail.com>
2024-07-27 13:55:54 +0300

    Hosting Setted Up for Crud Proj

--- crudServer/realTimeApp.Crud.Console/Program.cs
+++ crudServer/realTimeApp.Crud.Console/Program.cs
@@ -1,9 +1,40 @@
-﻿namespace realTimeApp.Crud.Console;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace realTimeApp.Crud.Console;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        System.Console.WriteLine("Hello, World!");
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Build())
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        IHost host = Host.CreateDefaultBuilder(args)
+        .ConfigureServices((context, services) => {
+            ConfigureServices(services);
+        })
+        .UseSerilog()
+        .Build();
+
+        await host.StartAsync();
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder){
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true);
+    }
+
+    static void ConfigureServices(IServiceCollection services){
+
     }
 }
